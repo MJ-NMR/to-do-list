e6df92de82dbbc9fc8365386bfa76d82089f6bbc
mosab3482 <mow3482@gmail.com>
2026-02-09 19:00:03 +0200

    adam

---
+++ app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const connectDB = require("./db/connect");
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const cros = require("cors");
+app.use(
+  cros({
+    origin: "*",
+  }),
+);
+//middleware
+app.use(express.json());
+//routes
+app.use("/api/v1/tasks", tasks);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    console.log("connected to db");
+    app.listen(3000, console.log("server is running on port 3000"));
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+start();
